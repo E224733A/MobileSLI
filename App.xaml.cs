0841867e1d0f1f5356193440147c969e9d8287fc
E224733A <noebracq973@gmail.com>
2026-06-10 13:55:13 +0200

    Add documentation comments to App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,12 +1,24 @@
 namespace MobileSLI;
 
+/// <summary>
+/// Entry point of the MobileSLI application. This partial class derives from the MAUI Application class.
+/// It is responsible for initializing application components and creating the initial window.
+/// </summary>
 public partial class App : Application
 {
+    /// <summary>
+    /// Constructs the application and initializes its components.
+    /// </summary>
     public App()
     {
         InitializeComponent();
     }
 
+    /// <summary>
+    /// Overrides CreateWindow to create and return the main application window using AppShell.
+    /// </summary>
+    /// <param name="activationState">Activation state provided by the MAUI framework.</param>
+    /// <returns>A new Window instance containing the AppShell.</returns>
     protected override Window CreateWindow(IActivationState? activationState)
     {
         return new Window(new AppShell());
